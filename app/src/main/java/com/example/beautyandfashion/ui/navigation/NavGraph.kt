14fbe43987aea7920952ee9b88b3252cf1809fa1
hnndhr <hananadhira1475@gmail.com>
2025-06-11 15:03:27 +0700

    tambahin controller

--- app/src/main/java/com/example/beautyandfashion/ui/navigation/NavGraph.kt
+++ app/src/main/java/com/example/beautyandfashion/ui/navigation/NavGraph.kt
@@ -4,7 +4,10 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.example.beautyandfashion.ui.screen.features.BeautypediaScreen
 import com.example.beautyandfashion.ui.screen.features.BodyShapeScreen
+import com.example.beautyandfashion.ui.screen.features.ColorAnalysisScreen
+import com.example.beautyandfashion.ui.screen.features.SkinAnalysisScreen
 import com.example.beautyandfashion.ui.screen.home.HomeScreen
 import com.example.beautyandfashion.ui.screen.settings.SettingsScreen
 import com.example.beautyandfashion.ui.screen.wardrobe.WardrobeScreen
@@ -21,5 +24,11 @@ fun NavGraph(navController: NavHostController) {
         composable("wardrobe") { WardrobeScreen(navController) }
         composable("settings") { SettingsScreen(navController) }
         composable("body") { BodyShapeScreen(navController) }
+        composable("color") { ColorAnalysisScreen(navController) }
+        composable("skin") { SkinAnalysisScreen(navController) }
+        composable("wiki") { BeautypediaScreen(navController) }
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/beautyandfashion/ui/screen/features/ColorAnalysis.kt
@@ -0,0 +1,9 @@
+package com.example.beautyandfashion.ui.screen.features
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun `ColorAnalysisScreen`(
+    navController: NavController,
+){}
